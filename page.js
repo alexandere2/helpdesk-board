72962e32d68688a7d14833e47ac8f77eeb7770b4
alexandere2 <alexandere2@usf.edu>
2025-11-08 19:35:41 -0500

    Introduced Page.Js file

---
+++ page.js
@@ -0,0 +1,12 @@
+import Board from './components/Board';
+export default function HomePage() {
+    return (
+        <main className="mx-auto max-w-6xl p-6">
+        <h1 className="text-3xl font-bold mb-4">Helpdesk Ticket Board</h1>
+        <p className="text-sm text-gray-600 mb-6">
+            Filter by status and priority, search by keyword and add tickets to your queue.
+            </p>
+            <Board />
+        </main>
+    );
+}
