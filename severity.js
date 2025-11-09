41b58c4233f7f4476041fd76c3e3a8378eb676f9
alexandere2 <alexandere2@usf.edu>
2025-11-08 19:49:24 -0500

    Update severity.js

--- severity.js
+++ severity.js
@@ -1,1 +1,5 @@
+//src/app/lib/severity.js
 
+// Named utility for potential sorting or validation of order priority and status.
+export const priorityOrder = {Low: 1, Medium: 2, High: 3, Critical: 4};
+export const statusOrder = {'Open': 1, 'In Progress': 2, 'On Hold': 3, 'Resolved': 4};
