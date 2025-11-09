8013e0b37a2b1dc7d13c4961966053e2d9126745
alexandere2 <alexandere2@usf.edu>
2025-11-08 19:48:31 -0500

    Update StatusMessage.jsx

--- StatusMessage.jsx
+++ StatusMessage.jsx
@@ -1,1 +1,29 @@
+//src/app/components/StatusMessage.jsx
 
+'use client';
+
+// loading > Error > isEmpty
+export default function StatusMessage({ loading, error, isEmpty}) {
+    if (loading) {
+        return (
+            <div className="rounded border p-3 bg-yellow-50 text-yellow-900">
+                "Loading...""
+            </div>  
+        );
+    }
+    if (error) {
+        return (
+        <div className="rounded border p-3 bg-red-50 text-red-700">
+            "Unable to load tickets."
+        </div>
+        );
+    }
+    if (isEmpty) {
+        return (
+            <div className="rounded border p-3 bg-50 text blue-800">
+                "No tickets match your filters."
+            </div>
+        );
+    }
+    return null;
+}
