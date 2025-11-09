33662ec92600d06c8559ee049c00b91a793b953d
alexandere2 <alexandere2@usf.edu>
2025-11-08 19:47:13 -0500

    Update SearchBox.jsx

--- SearchBox.jsx
+++ SearchBox.jsx
@@ -1,1 +1,18 @@
+//src/app/components/SearchBox.jsx
 
+'user client';
+
+export default function SearchBox ({ value, onChange }) {
+    return (
+        <label className="block">
+            <span className="text-sm font-medium">Search</span>
+            <input 
+            type="string"
+            className="mt-1 w-full rounded border px-3 py-2"
+            placeholder="Search for title or description..."
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+    />
+    </label>
+    );
+}
