2998103feb1076f03619222e114b17f57526b120
alexandere2 <alexandere2@usf.edu>
2025-11-08 19:41:32 -0500

    Update TicketCard.jsx

--- TicketCard.jsx
+++ TicketCard.jsx
@@ -1,1 +1,23 @@
+//src/app/components/TicketCard.jsx
+
+'use client';
+import { priorityOrder, statusOrder } from '../lib/severity';
+//Displays the button and shows 
+export default function TicketCard({ title, description, priority, status, assignee, updatedAt, onAdd, inQueue }) {
+    return (
+        <div className="text-sm text-gray-500">
+            <h3 className="font-medium">{title}</h3>
+        <p className="text-sm">{description}</p>
+        <p className="text-sm">Priority: {priority}</p>
+        <p className="text-sm">Status: {status}</p>
+        <p className="text-sm text-gray-500">Updated: {new Date(updatedAt).toLocaleString()}</p>
+        <button
+        className="mt-2 bg blue-500 text white px-3 py-1 rounded disabled:bg-gray-400"
+        onClick={onAdd}
+        disabled={inQueue}>
+            {inQueue ? 'In Queue' : 'Add to Queue'}
+        </button>
+    </div>
+    );
+}
 
